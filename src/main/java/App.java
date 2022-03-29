7505572ead98c92519278248da1d996efc016813
Angelina Gerasenkova <gerasenlova2012@mail.ru>
2022-03-29 17:47:13 +0300

    init commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,13 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld bean2= (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2= applicationContext.getBean(HelloWorld.class);
 
         Cat bean3= (Cat) applicationContext.getBean("cat");
-        Cat bean4= (Cat) applicationContext.getBean("cat");
+        Cat bean4= applicationContext.getBean(Cat.class);
         System.out.println(bean==bean2);
         System.out.println(bean3==bean4);
 
-      System.out.println(bean.getMessage());
+        System.out.println(bean.getMessage());
+
     }
 }
